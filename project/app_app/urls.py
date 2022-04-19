e0e4de4f87cd4ac6d14c53f2195d6aab7689f310
pinballtec <daniilboiko99@gmail.com>
2022-04-19 11:47:56 +0200

    login is working

--- project/app_app/urls.py
+++ project/app_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.urls, name='urls'),
-    path('register', views.register_request, name='test'),
-    path("login", views.login_request, name="login"),
+    path('register/', views.register_request, name='test'),
+    path("login/", views.login_request, name="login"),
     path("entry-list", views.EntryListView.as_view(), name="entry-list"),
     path("entry/<int:pk>", views.EntryDetailView.as_view(), name="entry-detail"),
     path("create", views.EntryCreateView.as_view(), name="entry-create"),
--- project/app_app/views.py
+++ project/app_app/views.py
@@ -37,22 +37,15 @@ def register_request(request):
 
 
 def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
-                return redirect("app_app:homepage")
-            else:
-                messages.error(request, "Invalid username or password.")
-        else:
-            messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(request=request, template_name="app_app/login.html", context={"login_form": form})
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('entry-list')
+    return render(request, 'app_app/login.html', {'page': page})
 
 
 class EntryListView(ListView):
