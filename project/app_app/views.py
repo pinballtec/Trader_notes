9a215fd63155ff6601f8efe7b6e2416acc060298
pinballtec <daniilboiko99@gmail.com>
2022-04-19 17:29:09 +0200

    registration is finished

--- project/app_app/views.py
+++ project/app_app/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, redirect
 from .forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import (
     ListView,
     DetailView,
@@ -23,15 +24,19 @@ from django.views.generic import (
 # Create your views here.
 
 
+@csrf_exempt
 def register_request(request):
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful.")
-            return redirect("main:homepage")
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+            user = form.save(commit=False)
+            user.save()
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Registration successful.")
+                return redirect("login")
+            else:
+                messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request, 'app_app/register.html', context={"register_form": form})
 
