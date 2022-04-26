a88a30f3e596eff04f4c4d3573869d1a582e923e
Daniil_Boika <daniilboiko99@gmail.com>
2022-04-26 20:12:55 +0200

    frontend is finished

--- project/app_app/forms.py
+++ project/app_app/forms.py
@@ -13,6 +13,7 @@ class NewUserForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
+
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
